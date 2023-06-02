c4c5ccf86e76324689ab82219aa80f8f577c5c37
Minskoleopc <minskoleopc@gmail.com>
2023-06-02 20:41:16 +0530

    added

---
+++ script.js
@@ -0,0 +1,69 @@
+console.log("hello")
+
+// program 1
+let x = 10
+console.log(x)
+x = 200
+console.log(x)
+
+
+const y = 300
+console.log(y)
+
+
+// y = 600
+// program 2
+// addition       ------> +
+// subtraction    ------> -
+// multiplication -------> *
+// division       -------> /
+// modulus        -------> %
+
+
+let s =  10
+let t = 5
+
+console.log(s+t)
+console.log(s-t)
+console.log(s*t)
+console.log(s/t)
+console.log(s%t)
+
+let q = 8
+let r = 4
+
+console.log(q+r)
+console.log(q-r)
+console.log(q*r)
+console.log(q/r)
+console.log(q%r)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
