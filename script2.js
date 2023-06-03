8e6c93c582008cf8bd7b6759abbf0ac60b673ef1
Minskoleopc <minskoleopc@gmail.com>
2023-06-03 20:30:38 +0530

    added

---
+++ script2.js
@@ -0,0 +1,98 @@
+let x = 10
+console.log(x)
+x = 100
+console.log(x)
+
+// const 
+
+const y = 100
+console.log(y)
+//y = 899
+
+// Arithmetic 
+// + , - , * , / , %
+
+let a  = 10
+let b = 5
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(23%4) // 3
+
+let h = 100
+let j = 50
+
+console.log(h+j)
+console.log(h-j)
+console.log(h*j)
+console.log(h/j)
+console.log(h%j)
+
+// 10 pair --- 50 lines
+function Calculator(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+}
+Calculator(6,3)
+Calculator(12,4)
+
+
+//program 1
+
+// function without parameter and without return type
+
+function addA(){
+    console.log(9+9)
+}
+addA()
+addA()
+addA()
+addA()
+addA()
+
+// function with parameter and without return type
+function addB(x,y){
+    console.log(x+y)
+}
+addB(12,4) 
+addB(11,4)
+addB(1,4)
+
+// function with parameter and with return type
+
+// 100 - given 
+// 100 - show
+
+function addC(x,y){
+    return x + y
+}
+let q1 = addC(12,8)
+console.log(q1)
+console.log(q1+q1)
+console.log(q1 * 0.10)
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
