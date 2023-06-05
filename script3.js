0db76dc228be02988cb72ffb79e59c475f2b1ad9
Minskoleopc <minskoleopc@gmail.com>
2023-06-05 20:29:42 +0530

    added

---
+++ script3.js
@@ -0,0 +1,92 @@
+// Introvert   | Extrovert
+// calm        | loud
+// less outing | more outing
+// less social | more social
+
+// Human --- Type 
+// Properties - age , weight , height , color ,gender
+// Methods -  talk() , walk()
+
+// Vehicle 
+// Properties - color , type , model , regNo
+// Methods - start() , stop()
+
+// Bank type 
+// Properties - accNo , accName , bal , branchName
+// Method - deposit() , withdrawl()
+
+let x = 10
+console.log(x)
+console.log(typeof x)
+// 3 ,-4,0,-56,-67
+
+let y = true
+console.log(y)
+console.log(typeof y)
+// true or false
+
+let z = "chinmay"
+console.log(z)
+console.log(typeof z)
+//"A" , '1' , "chinmay" , "chin123" , " ","#","Anc21#"
+
+
+console.log(typeof 10) // number
+console.log(typeof '10') // string
+
+
+//comparison operator 
+
+// < , > , <= , >= ,!= , == ,=== ,!==
+// == !=       - value 
+// === , !==   - value and type
+
+console.log(4 > 3)
+console.log(10 == '10')
+console.log(10 === '10')
+console.log(10 !== '10')
+console.log(10 === 10)
+console.log(10 != '10')
+console.log(7 > 8)
+console.log( 8 < 9)
+console.log(7 >= 5 )
+console.log(6 <= 5 )
+console.log(7 >= 7)
+console.log(8 <= 8)
+
+
+// Logical operator 
+
+// && - AND
+
+// true   &&    true  -----> true
+// false  &&    true  -----> false
+// true   &&    false -----> false
+// false  &&    false -----> false
+
+// OR - ||
+
+// true   ||    true  -----> true
+// false  ||    true  -----> true
+// true   ||    false -----> true
+// false  ||    false -----> false
+
+
+// NOT = !
+// true  ---- false
+// false ---- true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
