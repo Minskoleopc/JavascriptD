2081e20e88f0bc43647eefaf08451f85b32a9162
Minskoleopc <minskoleopc@gmail.com>
2023-06-09 20:31:29 +0530

    added

---
+++ script6.js
@@ -0,0 +1,145 @@
+// program 1
+//let numT = -4
+
+// if(numT > 0 && numT <= 5){
+//     console.log('10 % discount')
+// }
+// if(numT > 5 && numT <= 10){
+//     console.log('20 % discount')
+// }
+// if(numT > 10){
+//     console.log('30 % discount')
+// }
+
+// program 3
+// if(numT > 0 && numT <= 5){
+//     console.log('10 % discount')
+// }
+// else if(numT > 5 && numT <= 10){
+//     console.log('20 % discount')
+// }
+// else if(numT > 10){
+//     console.log('30 % discount')
+// }
+// else {
+//     console.log('Incorrect input')
+// }
+
+
+// program 3
+//let marks  = 55
+
+//if(marks > 90){
+//     console.log('Grade A')
+// }
+// if(marks > 75){
+//     console.log('Grade B')
+// }
+// if(marks > 65){
+//     console.log('Grade C')
+// }
+
+// program 4
+// if(marks > 90){
+//     console.log('Grade A')
+// }
+// else if(marks > 75){
+//     console.log('Grade B')
+// }
+// else if(marks > 65){
+//     console.log('Grade C')
+// }
+// else {
+//     console.log('Fail')
+// }
+
+// program 5
+
+// let a = 10
+// let b = 5
+
+// if(a > b){
+//     console.log('a is greater')
+// }
+// else {
+//     console.log('b is greater')
+// }
+
+// program 6
+let x = 1000
+let y = 150
+let z = 20
+
+if(x > y && x > z){
+    console.log('x is greatest')
+}
+else if(y > x && y > z ){
+    console.log("y is greatest")
+}
+else {
+    console.log('z is greatest')
+}
+// program 7
+
+let s = 10
+let t = 5
+
+if(s > t){
+    console.log('s is greatest')
+}
+else {
+    console.log('t is greatest')
+}
+s > t ? console.log('s is greater'):console.log('t is greater')
+
+// program 8
+
+let age = 17
+let q1 = age >= 18 ? "can drive":"cannot drive"
+console.log(q1)
+
+// program 9
+
+let city = "indore"
+
+switch(city){
+    case 'pune':
+        console.log('MH')
+    case 'indore':
+        console.log('MP')
+    case 'jaipur':
+        console.log('RJ')
+    case 'lucknow':
+        console.log('UP')
+    default:
+        console.log('Incorrect city name')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
