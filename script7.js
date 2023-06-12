d5b9f4a67a47dcf5c87775db0901fa5c8ff0581d
Minskoleopc <minskoleopc@gmail.com>
2023-06-12 20:26:46 +0530

    added

---
+++ script7.js
@@ -0,0 +1,85 @@
+// without break statement
+
+
+// let city = "pune"
+// switch (city) {
+//     case 'pune':
+//         console.log('MH')
+//     case 'jaipur':
+//         console.log('RJ')
+//     case 'indore':
+//         console.log('MP')
+//     case 'lucknow':
+//         console.log('UP')
+//     default:
+//         console.log('Incorrect city name')
+// }
+
+// program 2
+// switch case with statement
+
+let city2 = "Pune"
+switch (city2) {
+    case 'pune':
+        console.log('MH')
+        break
+    case 'jaipur':
+        console.log('RJ')
+        break
+    case 'lucknow':
+        console.log('UP')
+        break
+    case 'indore':
+        console.log('MP')
+        break
+    default:
+        console.log('incorrect city name')
+}
+
+// program 3
+// let city3 = "bhopal"
+// switch (city3) {
+//     case 'pune':
+//     case 'nagpur':
+//         console.log('MH')
+//         break
+//     case 'jaipur':
+//     case 'udaipur':
+//         console.log('RJ')
+//         break
+//     case 'lucknow':
+//     case 'varasani':
+//         console.log('UP')
+//         break
+//     case 'indore':
+//     case 'bhopal':
+//         console.log('MP')
+//         break
+//     default:
+//         console.log('incorrect city name')
+// }
+
+
+// truthy or false values 
+
+// true values  ---> 1, -1 ," ","chinmay","A","123,{},[], 2 == 2 ,true
+// false values ---> 0 , undefined , null , NaN , "", 4 < 3 , false
+
+// let a ;
+// console.log(a)
+
+if(false){
+    console.log("hello")
+}
+else {
+    console.log('bye')
+}
+
+// loops --- for 
+// while loop
+
+
+
+
+
+
