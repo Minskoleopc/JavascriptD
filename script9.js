c432460c0c498736f57d6dc77c6f710249d71322
Minskoleopc <minskoleopc@gmail.com>
2023-06-13 20:30:28 +0530

    added

---
+++ script9.js
@@ -0,0 +1,60 @@
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+
+// for and while loop
+
+// for (intialization; conditionCheck; increment / decrement) {
+//     //statements
+// }
+
+// program -- 1 to 3
+
+for (let i = 1; i <= 3; i++) {  // 2 // 3 // 4
+    console.log(i)  //1 // 2 // 3
+}
+// 1 <= 3 --- true
+// 2 <= 3 --- true
+// 3 <= 3 --- true 
+// program 2
+// print hello 3 times 
+for (let i = 1; i <= 3; i++) { // 2 // 3 // 4
+    console.log("hello")
+
+    // hello
+    // hello
+    // hello
+}
+
+// program 3
+for(let i = 1 ; i <= 5 ; i++){ // 2 // 3 // 4 // 5 // 6
+    console.log(i) // 1 // 2 // 3 // 4 // 5
+}
+
+// program 4
+for(let i = 5 ; i >= 1 ; i--){ // 4 // 3 // 2 // 1 // 0
+    console.log(i) // 5 // 4 // 3 // 2 // 1
+}
+
+// program 5
+ for(i = 2 ; i <= 20 ; i= i + 2){ // 4 // 6 // 8 // 10 // 12 // 14 // 16 // 18 // 20 // 22
+    console.log(i) // 2 // 4 // 6 // 8 // 10 // 12 // 14 // 16 // 18 // 20
+ }
+
+ // program 6
+ for(let i = 50 ; i >= 5 ; i = i - 5){
+    console.log(i)
+ }
+
+
+ // for with break 
+ // for with continue
+
+
+
+
+
+
+
