f0f3697041fa7a94c77e57a631aecf6c5fb712ce
Minskoleopc <minskoleopc@gmail.com>
2023-06-25 21:35:42 +0530

    added

---
+++ DOM/script.js
@@ -0,0 +1,5 @@
+document.querySelector('h1').addEventListener('click',function(){
+    document.querySelector('h1').style.color = "purple";
+    document.querySelector('h1').style.backgroundColor = "black";
+    document.querySelector('h1').textContent = "tanmay";
+})
