f23db97ecd215bfa59b112eadc5ce648889445ce
Minskoleopc <minskoleopc@gmail.com>
2023-07-01 09:03:55 +0530

    added

---
+++ DOM12/script.js
@@ -0,0 +1,49 @@
+// Program 1 (index.html)
+
+// let headOne = document.querySelector('h1')
+// console.log(headOne)
+
+// headOne.addEventListener('click',function(){
+//     headOne.textContent = "Maya"
+//     headOne.style.color = "red"
+//     headOne.style.background = "yellow"
+// })
+
+
+// program 2 (index2.html)
+
+// let headOne = document.querySelector('h1')
+// let buttonOne = document.querySelector('button')
+
+// buttonOne.addEventListener('click',function(){
+//     headOne.textContent = "Maya"
+//         headOne.style.color = "red"
+//     headOne.style.background = "yellow"
+// })
+
+
+
+
+// program 3(index3.html)
+
+// let buttonA = document.querySelector('button')
+// let input = document.querySelector('input')
+// let headone = document.querySelector('h1')
+
+// console.log(input)
+// console.log(buttonA)
+// console.log(headone)
+
+// buttonA.addEventListener('click',function(){
+//     let cltxt = input.value
+//     headone.style.color = cltxt
+//     input.value  = ""
+// })
+
+
+
+
+
+
+
+
