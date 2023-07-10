7895cfb539ffbb37f6c707e9601e6017179cd2d3
Minskoleopc <minskoleopc@gmail.com>
2023-07-10 21:01:08 +0530

    added

---
+++ DOM20/script.js
@@ -0,0 +1,69 @@
+// program 1
+
+// let liList = document.querySelectorAll('li')
+// console.log(liList)
+
+// for (let i = 0; i < liList.length; i++) {
+//     // console.log(i)
+//     if (i % 2 == 0) {
+//         liList[i].style.color = "green"
+//     }
+//     else {
+//         liList[i].style.color = "purple"
+//     }
+// }
+
+// program 2
+
+
+// let ulList = document.querySelector('ul')
+// // let liList = document.querySelectorAll('li')
+// // ulList.addEventListener('mouseover',function(){
+// //     for(let i = 0 ; i < liList.length; i++){
+// //         liList[i].style.color = "green"
+// //     }
+
+// // })
+
+// // ulList.addEventListener('mouseout',function(){
+// //     for(let i = 0 ; i < liList.length; i++){
+// //         liList[i].style.color = "red"
+// //     }
+
+// // })
+
+
+
+// let ulList = document.querySelector('ul')
+// let liList = document.querySelectorAll('li')
+// ulList.addEventListener('click',function(){
+//     for(let i = 0 ; i < liList.length; i++){
+//         liList[i].style.color = "green"
+//     }
+
+// })
+
+// ulList.addEventListener('dblclick',function(){
+//     for(let i = 0 ; i < liList.length; i++){
+//         liList[i].style.color = "purple"
+//     }
+
+// })
+
+
+let r = document.querySelector('#one')
+
+r.addEventListener('click',function(){
+    location.reload()
+})
+
+
+
+
+
+
+
+
+
+
+
